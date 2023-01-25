d65886d53b2e6e06d36c62af2778693378b097e2
lea-dubois1 <lea.dubois@laplateforme.io>
2023-01-25 10:26:10 +0100

    galerie terminée

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body>
+
+
+        <section class="hero-section">
+            <div class="card-grid">
+
+                <a class="card" href="#">
+                    <div class="card__background card8"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">JAVIER 2022</p>
+                        <h3 class="card__heading">Memory</h3>
+                    </div>
+                </a>
+
+                <a class="card" href="#">
+                    <div class="card__background card7"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">DECEMBRE 2022</p>
+                        <h3 class="card__heading">My CMS</h3>
+                    </div>
+                </a>
+
+                <a class="card" href="#">
+                    <div class="card__background card6"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">DECEMBRE 2022</p>
+                        <h3 class="card__heading">Réservation de salles</h3>
+                    </div>
+                </a>
+
+                <a class="card" href="#">
+                    <div class="card__background card5"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">DECEMBRE 2022</p>
+                        <h3 class="card__heading">Livre d'or</h3>
+                    </div>
+                </a>
+
+                <a class="card" href="#">
+                    <div class="card__background card4"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">DECEMBRE 2022</p>
+                        <h3 class="card__heading">Module de connexion</h3>
+                    </div>
+                </a>
+
+                <a class="card" href="#">
+                    <div class="card__background card3"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">NOVEMBRE 2022</p>
+                        <h3 class="card__heading">Voyages</h3>
+                    </div>
+                </a>
+
+                <a class="card" href="#">
+                    <div class="card__background card2"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">NOVEMBRE 2022</p>
+                        <h3 class="card__heading">App favorites</h3>
+                    </div>
+                </a>
+
+                <a class="card" href="#">
+                    <div class="card__background card1"></div>
+                    
+                    <div class="card__content">
+                        <p class="card__category">OCTOBRE 2022</p>
+                        <h3 class="card__heading">site La Plateforme</h3>
+                    </div>
+                </a>
+
+            <div>
+        </section>
+            
+    </body>
+</html>
