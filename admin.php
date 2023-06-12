529bda53d4891ac78ca57eb89caf43e359239002
lea-dubois1 <lea.dubois@laplateforme.io>
2023-06-12 21:04:02 +0200

    create admin form

---
+++ admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel admin</title>
+</head>
+
+<body>
+
+    <header></header>
+
+    <main>
+        <form action="" method="GET">
+            <input type="text" name="titre" id="titre" placeholder="Titre" />
+            <input type="text" name="Stitre" id="Stitre" placeholder="Small titre" />
+            <textarea name="resume" id="resume" placeholder="Résumé"></textarea>
+            <input type="text" name="fichier" id="fichier" placeholder="Nom fichier" />
+            <input type="text" name="github" id="github" placeholder="Lien GitHub" />
+            <input type="date" name="date" id="date" />
+            <button type="submit">Publier</button>
+        </form>
+    </main>
+
+    <footer></footer>
+
+</body>
+
+</html>
